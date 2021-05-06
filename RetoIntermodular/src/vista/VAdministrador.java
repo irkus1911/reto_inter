e58bc3ddd4376918f677611b3715bf9e713e8a2e
1dam <1dam@TartangaLH.eus>
2021-05-06 12:09:25 +0200

    Código Java v4

--- RetoIntermodular/src/vista/VAdministrador.java
+++ RetoIntermodular/src/vista/VAdministrador.java
@@ -2,18 +2,25 @@ package vista;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JButton;
 import javax.swing.JSpinner;
 import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
 
-public class VAdministrador extends JFrame {
+public class VAdministrador extends JFrame implements ActionListener {
 
 	private JPanel contentPane;
-
+	private JButton btnCerrarSesion;
+	private JButton btnAcceso;
+	private JComboBox comboTipoUsuarioAlta ;
+	private JButton btnAlta;
 	/**
 	 * Launch the application.
 	 */
@@ -41,20 +48,63 @@ public class VAdministrador extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JButton btnAlta = new JButton("Alta");
+		 btnAlta = new JButton("Alta");
 		btnAlta.setBounds(304, 206, 89, 23);
 		contentPane.add(btnAlta);
 		
-		JComboBox comboTipoUsuarioAlta = new JComboBox();
+		 comboTipoUsuarioAlta = new JComboBox();
+		comboTipoUsuarioAlta.setModel(new DefaultComboBoxModel(new String[] {"Cliente", "Comercio", "Suministrador"}));
+		comboTipoUsuarioAlta.setSelectedIndex(-1);
 		comboTipoUsuarioAlta.setBounds(85, 109, 270, 22);
 		contentPane.add(comboTipoUsuarioAlta);
 		
-		JButton btnAcceso = new JButton("Acceder");
+		 btnAcceso = new JButton("Acceder");
 		btnAcceso.setBounds(67, 206, 89, 23);
 		contentPane.add(btnAcceso);
 		
-		JButton btnCerrarSesion = new JButton("CerrarSesion");
+		 btnCerrarSesion = new JButton("CerrarSesion");
 		btnCerrarSesion.setBounds(10, 11, 120, 23);
 		contentPane.add(btnCerrarSesion);
+		btnCerrarSesion.addActionListener(this);
+		btnAcceso.addActionListener(this);
+		btnAlta.addActionListener(this);
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if(e.getSource().equals(btnCerrarSesion)) {
+			//posible option pane(pop-up)
+			VBienvenida vBienve=new VBienvenida();
+			vBienve.setVisible(true);
+			this.dispose();
+		}else if(e.getSource().equals(btnAcceso)){
+			if(comboTipoUsuarioAlta.getSelectedIndex()==0) {
+				VCliente vClie=new VCliente();
+				vClie.setVisible(true);
+				this.dispose();
+				
+			}else if(comboTipoUsuarioAlta.getSelectedIndex()==1) {
+				VComercio vComer=new VComercio();
+				vComer.setVisible(true);
+				this.dispose();
+				
+			}else if(comboTipoUsuarioAlta.getSelectedIndex()==2) {
+				VSuministrador vSumi=new VSuministrador();
+				vSumi.setVisible(true);
+				this.dispose();
+				
+			}else {
+				JOptionPane.showMessageDialog(btnAcceso, "Error seleccion incorrecta", "ERROR",
+						JOptionPane.ERROR_MESSAGE);
+			}
+			
+		}else {
+			//Pasar esto a otra pestaña
+			
+			
+		}
+		
 	}
 }
--- RetoIntermodular/src/vista/VBienvenida.java
+++ RetoIntermodular/src/vista/VBienvenida.java
@@ -10,11 +10,16 @@ import javax.swing.border.EmptyBorder;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
 
-public class VBienvenida extends JDialog {
+public class VBienvenida extends JDialog implements ActionListener {
 
 	private final JPanel contentPanel = new JPanel();
+	private JComboBox comboTipoUsuarioLogin;
+	private JButton btnIrALogin;
+	
 
 	/**
 	 * Launch the application.
@@ -38,17 +43,57 @@ public class VBienvenida extends JDialog {
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
 		contentPanel.setLayout(null);
-		
-		JButton btnIrALogin = new JButton("Continuar");
+
+		btnIrALogin = new JButton("Continuar");
 		btnIrALogin.setBounds(297, 194, 89, 23);
 		contentPanel.add(btnIrALogin);
-		
+
 		JLabel lblBienvenida = new JLabel("Bienvenide les gallines");
 		lblBienvenida.setBounds(158, 41, 137, 14);
 		contentPanel.add(lblBienvenida);
-		
-		JComboBox comboTipoUsuarioLogin = new JComboBox();
+
+		comboTipoUsuarioLogin = new JComboBox();
+		comboTipoUsuarioLogin.setModel(
+				new DefaultComboBoxModel(new String[] { "Administrador", "Cliente", "Comercio", "Suministrador" }));
+		comboTipoUsuarioLogin.setSelectedIndex(-1);
 		comboTipoUsuarioLogin.setBounds(126, 123, 182, 22);
 		contentPanel.add(comboTipoUsuarioLogin);
+		
+
+		btnIrALogin.addActionListener(this);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if (comboTipoUsuarioLogin.getSelectedIndex() == 0) {
+			if (e.getSource().equals(btnIrALogin)) {
+				VLogin login=new VLogin("Administrador");
+				login.setVisible(true);
+				this.dispose();
+
+			}
+		} else if (comboTipoUsuarioLogin.getSelectedIndex() == 1) {
+			if (e.getSource().equals(btnIrALogin)) {
+				VLogin login=new VLogin("Cliente");
+				login.setVisible(true);
+				this.dispose();
+			}
+		} else if (comboTipoUsuarioLogin.getSelectedIndex() == 2) {
+			if (e.getSource().equals(btnIrALogin)) {
+				VLogin login=new VLogin("Comercio");
+				login.setVisible(true);
+				this.dispose();
+			}
+		} else if (comboTipoUsuarioLogin.getSelectedIndex() == 3) {
+			if (e.getSource().equals(btnIrALogin)) {
+				VLogin login=new VLogin("Suministrador");
+				login.setVisible(true);
+				this.dispose();
+			}
+		} else {
+			JOptionPane.showMessageDialog(btnIrALogin, "Error seleccion incorrecta", "ERROR",
+					JOptionPane.ERROR_MESSAGE);
+		}
 	}
 }
